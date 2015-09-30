e846cbda61512698eebffd806262d4eb64d1feba
Golker <lucabezerra@gmail.com>
2015-09-29 23:00:09 -0300

    Pós-Defesa de Mestrado

--- MidiProcessor.py
+++ MidiProcessor.py
@@ -2,155 +2,241 @@
 
 __author__ = 'Luca'
 
+import math
+from collections import OrderedDict
 import mido
 from mido import midifiles
-from collections import OrderedDict
-import math
 import PercussionDevicesEnum
-from time import gmtime, strftime, localtime
 
-# mid = None
-# fileName = ""
-# beats = {}
-# instruments = {}
-# orderedBeats = {}
-# numerator = denominator = 4
-# tempo = absoluteTimeCounter = ticksPerBeat = maxTrackLength = barSize = barCounter = 0
 
 class MidiProcessor(object):
-	def __init__(self, midi_file_path, file_name, runtime):
-		self.mid = mido.midifiles.MidiFile(midi_file_path)
-		self.fileName = file_name
-		self.runtime = runtime
-		self.ticksPerBeat = self.mid.ticks_per_beat
-		#self.mid.print_tracks()
-		####
-		self.beats = {}
-		self.instruments = {}
-		self.orderedBeats = {}
-		self.numerator = self.denominator = 4
-		self.tempo = self.absoluteTimeCounter = self.maxTrackLength = self.barSize = self.barCounter = 0
+    '''
+        Class that performs the whole MIDI processing.
+    '''
 
-	def processTracks(self):
-		for i, track in enumerate(self.mid.tracks):
-			self.absoluteTimeCounter = 0
-			for j, message in enumerate(track):
-				self.absoluteTimeCounter += message.time  # not only note_on messages have times greater than zero
-				if message.type == 'note_on':
-					if message.velocity > 0:
-						# The same absolute time might have beats from
-						# multiple instruments. Therefore, a list is required.
-						if self.absoluteTimeCounter not in self.beats:
-							self.beats[self.absoluteTimeCounter] = []
-							self.beats[self.absoluteTimeCounter].append(message)
-						else:
-							hasNote = False
-							# Check if there are no other messages from the same instrument at that same time before inserting
-							for entry in self.beats[self.absoluteTimeCounter]:
-								if entry.note == message.note:
-									hasNote = True
-							if not hasNote:
-								self.beats[self.absoluteTimeCounter].append(message)
-				elif message.type == 'set_tempo':
-					tempo = message.tempo
-					bpm = (1000000 / tempo) * 60
-				elif message.type == 'time_signature':
-					numerator = message.numerator
-					denominator = message.denominator
-				elif message.type == 'text':
-					pass
-			if self.absoluteTimeCounter > self.maxTrackLength:
-				self.maxTrackLength = self.absoluteTimeCounter
+    def __init__(self, midi_file_path, filename, runtime, print_instruments_data=False,
+                 generate_image_files=False, only_two_instruments=False, filter_by_timber=False):
+        self.mid = mido.midifiles.MidiFile(midi_file_path)
+        self.filename = filename
+        self.runtime = runtime
+        self.ticks_per_beat = self.mid.ticks_per_beat
+        # self.mid.print_tracks()
+        self.beats = {}
+        self.instruments = {}
+        self.ordered_beats = {}
+        self.numerator = self.denominator = 4
+        self.tempo, self.absolute_time_counter, self.max_track_length, \
+        self.bar_size, self.bar_counter, self.bpm = 0, 0, 0, 0, 0, 0
+        self.print_instruments_data = print_instruments_data
+        self.generate_image_files = generate_image_files
+        self.only_two_instruments = only_two_instruments
+        self.filter_by_timber = filter_by_timber
+        self.percussion_devices = PercussionDevicesEnum.PercussionDevices()
 
-	def setup_variables(self):
-		self.orderedBeats = OrderedDict(sorted(self.beats.items()))  # Python's dictionaries are not ordered.
-		#self.barSize = getTubsPlacement(self.ticksPerBeat) * self.denominator
-		#self.barCounter = 0
-		#print "TS: " + str(self.numerator) + "/" + str(self.denominator) + "(" + str(self.barSize) + ")"
-		#print "T: " + str(self.tempo) + " | BPM: " + str(self.bpm) + " | TPB: " + str(self.ticksPerBeat)
+    def process_tracks(self):
+        for i, track in enumerate(self.mid.tracks):
+            self.absolute_time_counter = 0
+            for j, message in enumerate(track):
+                # not only note_on messages have times greater than zero
+                self.absolute_time_counter += message.time
+                if message.type == 'note_on':
+                    if message.velocity > 0 and message.channel == 9:
+                        # The same absolute time might have beats from
+                        # multiple instruments. Therefore, a list is required.
+                        if self.absolute_time_counter not in self.beats:
+                            self.beats[self.absolute_time_counter] = []
+                            self.beats[self.absolute_time_counter].append(message)
+                        else:
+                            has_note = False
+                            # Check if there are no other messages from the same instrument at that
+                            # same time before inserting
+                            for entry in self.beats[self.absolute_time_counter]:
+                                if entry.note == message.note:
+                                    has_note = True
+                            if not has_note:
+                                self.beats[self.absolute_time_counter].append(message)
+                elif message.type == 'set_tempo':
+                    self.tempo = message.tempo
+                    self.bpm = (1000000 / self.tempo) * 60
+                elif message.type == 'time_signature':
+                    self.numerator = message.numerator
+                    self.denominator = message.denominator
+                elif message.type == 'text':
+                    pass
+            if self.absolute_time_counter > self.max_track_length:
+                self.max_track_length = self.absolute_time_counter
 
-	def beatsByInstrument(self):
-		for absBeatTime, instrumentsList in self.orderedBeats.iteritems():
-			for singleInstrument in instrumentsList:
-				instrumentName = str(singleInstrument.note)
-				if instrumentName not in self.instruments:
-					self.instruments[instrumentName] = []
-				self.instruments[instrumentName].append(absBeatTime)
+    def setup_variables(self):
+        # Python's dictionaries are unordered.
+        self.ordered_beats = OrderedDict(sorted(self.beats.items()))
 
-	def printInstrumentName(self, instID):
-		if int(instID) >= 33 and int(instID) <= 81:
-			print PercussionDevicesEnum.PercussionDevices.PercussionDevicesDict[int(instID)]
-		else:
-			print instID
+    def beats_by_instrument(self):
+        for abs_beat_time, instruments_list in self.ordered_beats.iteritems():
+            for single_instrument in instruments_list:
+                instrument_name = str(single_instrument.note)
+                if instrument_name not in self.instruments:
+                    self.instruments[instrument_name] = []
+                self.instruments[instrument_name].append(abs_beat_time)
 
-	def getTubsPlacement(self, attackTime):
-		return int(math.ceil(float(attackTime) / (float(self.ticksPerBeat) / 12.0)))
-		#return attackTime
+    def count_instruments_by_beat(self):
+        inst_sum = 0
+        binary_sum = 0
+        max_instruments_at_once = 1
+        min_instruments_at_once = len(self.instruments)
+        means = []
 
-	# "instruments" é um dicionário onde, para cada elemento, a chave é o nome do instrumento,
-	# e o valor é uma lista de inteiros, onde cada elemento representa o tempo, em ticks, onde
-	# aconteceu um ataque de nota. Para transformar na notação TUBS, é preciso preencher as
-	# diferenças de tempo entre cada ataque com espaços vazios (ou pontos, na notação textual).
-	def createTimelines(self):
-		#print "\nINSTRUMENTOS:"
-		for instrumentID, beatsTicks in self.instruments.iteritems():
-			#self.printInstrumentName(instrumentID)
-			prevTick = 0
-			tubs = ""
-			prevDiff = 0
-			# Deve-se preencher espaços vazios apenas nos timeslots que não sejam utilizados por batidas. Por exemplo,
-			# se temos batidas nos tempos 4 e 7 do sistema TUBS, devemos preencher com [7 - (4+1)] = 2 batidas, pois os
-			# tempos 4 e 7 já estão reservados, sobrando os tempos 5 e 6 para preencher com espaços vazios.
-			for tick in beatsTicks:
-				tubsTick = self.getTubsPlacement(tick)
-				tubsPrevTick = self.getTubsPlacement(prevTick)
-				# if tubsTick > (barSize * (barCounter + 1)):
-				#	 tubs += "|"
-				#	 barCounter += 1
+        if len(self.instruments) < 2:
+            print "LESS THAN 2 INSTRUMENTS:", self.mid.filename
 
-				for _ in range(tubsPrevTick + 1, tubsTick):
-					tubs += "."
+        # gets the list of instruments that played at each beat
+        for beat, inst_list in self.ordered_beats.iteritems():
+            # checks and sets the max and min instruments found
+            if len(inst_list) > max_instruments_at_once: max_instruments_at_once = len(inst_list)
+            if len(inst_list) < min_instruments_at_once: min_instruments_at_once = len(inst_list)
 
-				diff = len(tubs) + 1 - self.getTubsPlacement(tick)
-				#if not tubsPrevTick == tubsTick and not len(tubs) + 1 == self.getTubsPlacement(tick) and not diff == prevDiff and tick > 0:
-				if tubsPrevTick == tubsTick and tick > 0:
-					#print "HERE \/! Diff: " + str(diff)
-					prevDiff = diff
-					#tubs += "|"
-					#tubs += "X"
-				else:
-					tubs += "X"
+            if len(inst_list) >= 2:
+                binary_sum += 1
 
-				#if instrumentID == '42':
-				#	print "Len: " + str(len(tubs)) + " | Pos (Tick): " + str(self.getTubsPlacement(tick))
+            # checks whether it should filter the coincidences by timber or not
+            if not self.filter_by_timber:
+                inst_sum += len(inst_list)
+            else:
+                inst_sum += self.percussion_devices.get_timber_group(inst_list)
 
-				if tick == 0:
-					prevTick = tick + 1
-				else:
-					prevTick = tick
+        # The Arithmetic Mean (sum of instruments from all beats divided by the amount of beats)
+        arith_mean = float(inst_sum) / float(len(self.ordered_beats))
 
-			# Preenchendo os últimos slots do TUBS, caso a última batida não tenha sido no último tempo da música. Aqui
-			#  não é necessário o lembrete de cima, com relação à subtração de 1 do valor, porque o valor de
-			# maxTrackLength é o último slot a ser preenchido, não a próxima batida.
-			if self.getTubsPlacement(prevTick) < self.getTubsPlacement(self.maxTrackLength):
-				for t in range(self.getTubsPlacement(prevTick) + 1, self.getTubsPlacement(self.maxTrackLength) + 1):
-					tubs += "."
+        return_data = ReturnData()
+        return_data.mean_type = 'am'
+        return_data.num_insts = len(self.instruments)
+        return_data.mean_value = arith_mean
 
-			#print tubs + "\t" + str(self.getTubsPlacement(prevTick)) + "-" + str(self.getTubsPlacement(self.maxTrackLength)),
-			#print "[Length: " + str(len(tubs)) + "]"
+        means.append(return_data)
 
-			if len(tubs) != self.getTubsPlacement(self.maxTrackLength):
-				print "**************** Problem on timeline length: " + self.mid.filename + " -->",
-				str(self.printInstrumentName(instrumentID))
-			elif len(self.instruments) < 2:
-				print "**************** Less than 2 instruments: " + self.mid.filename + " -->" +\
-				str(len(self.instruments))
+        # The Binary Mean (anything above 1 instrument counts as only 2 instruments)
+        bin_mean = min_instruments_at_once + (float(binary_sum) / float(len(self.ordered_beats)))
+
+        return_data = ReturnData()
+        return_data.mean_type = 'bm'
+        return_data.num_insts = len(self.instruments)
+        return_data.mean_value = bin_mean
+
+        means.append(return_data)
+
+        return means
+
+    def print_instrument_name(self, inst_id):
+        if int(inst_id) >= 33 and int(inst_id) <= 81:
+            print self.percussion_devices.PercussionDevicesDict[int(inst_id)]
+        else:
+            print inst_id
+
+    def get_tubs_placement(self, attack_time):
+        return int(math.ceil(float(attack_time) / (float(self.ticks_per_beat) / 12.0)))
+
+    def create_timelines(self):
+        # "instruments" é um dicionário onde, para cada elemento, a chave é o nome do instrumento,
+        # e o valor é uma lista de inteiros, onde cada elemento representa o tempo, em ticks, onde
+        # aconteceu um ataque de nota. Para transformar na notação TUBS, é preciso preencher as
+        # diferenças de tempo entre cada ataque com espaços vazios (ou pontos, na notação textual).
+        if self.print_instruments_data:
+            print "\nINSTRUMENTOS:"
+        for instrument_id, beats_ticks in self.instruments.iteritems():
+            if self.print_instruments_data and len():
+                self.print_instrument_name(instrument_id)
+            prev_tick = 0
+            tubs = ""
+            # Deve-se preencher espaços vazios apenas nos timeslots que não sejam utilizados por batidas. Por exemplo,
+            # se temos batidas nos tempos 4 e 7 do sistema TUBS, devemos preencher com [7 - (4+1)] = 2 batidas, pois os
+            # tempos 4 e 7 já estão reservados, sobrando os tempos 5 e 6 para preencher com espaços vazios.
+            for tick in beats_ticks:
+                tubs_tick = self.get_tubs_placement(tick)
+                tubs_prev_tick = self.get_tubs_placement(prev_tick)
+
+                for _ in range(tubs_prev_tick + 1, tubs_tick):
+                    tubs += "."
+
+                if tubs_prev_tick == tubs_tick and tick > 0:
+                    pass
+                else:
+                    tubs += "X"
+
+                if tick == 0:
+                    prev_tick = tick + 1
+                else:
+                    prev_tick = tick
+
+            # Preenchendo os últimos slots do TUBS, caso a última batida não tenha sido
+            # no último tempo da música.
+            if self.get_tubs_placement(prev_tick) < self.get_tubs_placement(self.max_track_length):
+                for t in range(self.get_tubs_placement(prev_tick) + 1,
+                               self.get_tubs_placement(self.max_track_length) + 1):
+                    tubs += "."
+
+            if self.print_instruments_data:
+                print tubs + "\t" + str(self.get_tubs_placement(prev_tick)) + "-" + \
+                      str(self.get_tubs_placement(self.max_track_length)),
+                print "[Length: " + str(len(tubs)) + "]"
+
+        if self.generate_image_files:
+            return self.count_instruments_by_beat()
+
+        return ""
 
 
-	def writeResultsToFile(self):
-		line = self.fileName + "\t\t" + str(self.maxTrackLength) + "\t\t" + str(len(self.instruments)) + \
-		       "\t\t" + str(self.numerator) + "/" + str(self.denominator) + "\t\t" + str(self.ticksPerBeat) + "\n"
+    def format_results_for_file_writing(self):
+        return self.filename + "\t\t" + str(self.max_track_length) + "\t\t" + \
+               str(len(self.instruments)) + "\t\t" + str(self.numerator) + "/" + \
+               str(self.denominator) + "\t\t" + str(self.ticks_per_beat) + "\n"
 
-		with open(self.runtime + ".txt", 'a') as f:
-			f.write(line)
 
+    def normalize_array(self, original_array):
+        return_array = []
+        max_value = max(original_array)
+        min_value = min(original_array)
+        normalized_max = 1.0
+        normalized_min = -1.0
+        normalized_range = normalized_max - normalized_min
+
+        # to avoid a ZeroDivisionError below, we deal with this special case
+        if max_value == min_value:
+            return [normalized_min for _ in range(len(original_array))]
+
+        # lambda function for transposing the original values to the normalized ones through a
+        # linear function calculation
+        # y = ax + b -> normalized_value = (a * original_value) + b
+        a = -normalized_range / float(min_value - max_value)
+        b = normalized_max - (a * max_value)
+        converter = lambda x: (a * x) + b
+
+        for item in original_array:
+            return_array.append(converter(item))
+
+        return return_array
+
+
+class ReturnData(object):
+    mean_type = ""
+    num_insts = 0
+    mean_value = 0.0
+
+
+if __name__ == '__main__':
+    from time import localtime, strftime
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Running the processor module by itself.')
+    parser.add_argument('-file', '-f', type=str, help='the file path')
+
+    args = parser.parse_args()
+    print "Begin: " + str(strftime("%Y_%m_%d___%H_%M_%S", localtime()))
+    processor = MidiProcessor(args.file, args.file.split('\\')[-1],
+                              strftime("%Y_%m_%d___%H_%M_%S", localtime()),
+                              print_instruments_data=True, generate_image_files=True,
+                              only_two_instruments=False, filter_by_timber=False)
+    processor.process_tracks()
+    processor.setup_variables()
+    processor.beats_by_instrument()
+    means = processor.create_timelines()
+    processor.format_results_for_file_writing()
+    print "End: " + str(strftime("%Y_%m_%d___%H_%M_%S", localtime()))
--- main.py
+++ main.py
@@ -2,26 +2,186 @@
 
 __author__ = 'Luca'
 
-import MidiProcessor
-from time import localtime, strftime
 import os
+from time import localtime, strftime
+from collections import OrderedDict
 
-windows_url = "C:\\Users\\Luca\\PycharmProjects\\Testing\\MIDIs"
-linux_url = ""
+import matplotlib.pyplot as plt
+import numpy as np
+
+import MidiProcessor
+
+
+class PlotGenerator():
+    WINDOWS_DIRPATH = "C:\\Users\\Luca\\PycharmProjects\\Testing\\MIDIs"
+    LINUX_DIRPATH = ""
+    FILE_EXTENSION = ".MID"
+
+    def __init__(self):
+        self.file_contents = ""
+
+    def get_time(self):
+        return strftime("%Y_%m_%d___%H_%M_%S", localtime())
+
+    def run(self):
+        runtime = self.get_time()
+        print "Begin: " + str(runtime)
+
+        amount_of_means_by_num_insts = dict()
+        amount_of_means_by_num_insts['am'] = dict()
+        amount_of_means_by_num_insts['bm'] = dict()
+
+        for (dirpath, dirnames, filenames) in os.walk(self.WINDOWS_DIRPATH):
+            for name in filenames:
+                # remove the second condition on this IF to include drum samples
+                if str(name).upper().find(self.FILE_EXTENSION) != -1:  # and dirpath.find("Drum_Loops") == -1:
+                    self.processor = MidiProcessor.MidiProcessor(
+                        os.path.join(dirpath + "\\" + name), name, runtime,
+                        print_instruments_data=False, generate_image_files=True,
+                        only_two_instruments=False, filter_by_timber=False)
+                    self.processor.process_tracks()
+                    self.processor.setup_variables()
+                    self.processor.beats_by_instrument()
+                    means = self.processor.create_timelines()
+                    self.file_contents += self.processor.format_results_for_file_writing()
+
+                    if means != "":
+                        for i in range(len(means)):
+                            # - means is a list with 2 ReturnData elements
+                            # - amount_of_means_by_num_insts is a dict with 'am' and 'bm' keys
+                            # - amount_of_means_by_num_insts['am'/'bm'] is a dict with the total #
+                            #   of instruments as its value
+                            # - amount_of_means_by_num_insts['am'/'bm'][6] is a dict with a list of
+                            #   means from files with that amount of instruments, rounded to
+                            #   4 digits
+
+                            # >>> print amount_of_means_by_num_insts['am'][7]
+                            # [1.0003, 1.0025, 1.0234]
+                            if means[i].num_insts not in \
+                                    amount_of_means_by_num_insts[means[i].mean_type]:
+                                amount_of_means_by_num_insts[means[i].mean_type][means[i]
+                                    .num_insts] = []
+
+                            amount_of_means_by_num_insts[means[i].mean_type][means[i].num_insts]\
+                                .append(round(means[i].mean_value, 4))
+
+        # Python's dictionaries are unordered.
+        ordered_means = OrderedDict(sorted(amount_of_means_by_num_insts.items()))
+        all_normed_means = []
+        tmp_dict = dict()
+
+        # populates the normed means list
+        for key, value in ordered_means['am'].iteritems():
+            am_overall_normed_means = []
+            x = value
+            if len(x) > 1:
+                tmp_dict[key] = []
+                for i in self.processor.normalize_array(x):
+                    am_overall_normed_means.append(i)
+                    all_normed_means.append(i)
+                    tmp_dict[key].append(i)
+
+                # for individual means use this. for cumulative, use the one outside the for loop
+                #fig = plt.figure(figsize=(8, 3))
+                #self.plot_data(am_overall_normed_means, key)
+
+        # plotting the cumulative means
+        fig = plt.figure(figsize=(8, 3))
+        self.plot_data(all_normed_means, None, plot_title="Distribuicao de Todas as Amostras (" +
+                                                         str(len(all_normed_means)) + " amostras)")
+
+        ordered_means_by_inst = OrderedDict()
+        for k, v in tmp_dict.iteritems():
+            print("Key: [" + str(k) + "] list: " + str(sorted(v)))
+            ordered_means_by_inst[k] = sorted(v)
+        #self.plot_color_gradients(len(ordered_means_by_inst), ordered_means_by_inst, 'Greys')
+
+        #self.write_to_file(runtime, self.file_contents)
+        print "End: " + str(self.get_time())
+
+    def plot_color_gradients(self, nrows, lists, cmap_type):
+        """
+        Plots a list of gradients with the given lists' distribution.
+        :param nrows: The number of entries (gradients) in the plot.
+        :param lists: The lists of data for each entry (gradient)
+        :param cmap_type: The colormap type (refer to
+        http://matplotlib.org/examples/color/colormaps_reference.html for options)
+        """
+        # gets the axis of each entry (gradient)
+        fig, axes = plt.subplots(nrows=nrows)
+        fig.subplots_adjust(top=0.95, bottom=0.01, left=0.2, right=0.99)
+        axes[0].set_title("'Means by Instrument' Colormap", fontsize=14)
+
+        # for each list of values contained in the 'lists' parameter
+        for i, (key, values_list) in enumerate(lists.iteritems()):
+            # generate the histogram values without plotting
+            #   bins: the histogram's intervals
+            #   vals: the values (y axis) for each interval
+            vals, bins = np.histogram(values_list)
+            # use values_list intead of vals below to get the distribution ordered by value
+            # 1st param must be a np.ndarray with 2 symmetric lists of values (for this case)
+            axes[i].imshow(np.concatenate((np.array([vals]), np.array([vals])),
+                                          axis=0), aspect='auto', cmap=plt.get_cmap(cmap_type))
+            pos = list(axes[i].get_position().bounds)
+            x_text = pos[0] - 0.01
+            y_text = pos[1] + pos[3] / 2.
+            # sets the text next to each entry
+            fig.text(x_text, y_text, str(key) + ' insts (' + str(len(values_list)) + ' samples)',
+                     va='center', ha='right', fontsize=10)
+
+        # Turn off *all* ticks & spines, not just the ones with colormaps.
+        for ax in axes:
+            ax.set_axis_off()
+
+        plt.show()
+
+    def plot_data(self, data_list, plot_id, plot_title=''):
+        """
+        Plots a histogram of the provided data.
+        :param data_list: list of values to be plotted
+        :param plot_id: in this case, the number of instruments that this plot corresponds to
+        :param plot_title: a title, if the standard format one isnt suitable
+        """
+        # plotting the histogram
+        n, bins, patches = plt.hist(data_list, alpha=0.3)
+        x_axis = []
+        # the points in the X axis are located exactly in the middle of each bin's interval
+        for i in range(len(bins[:-1])):
+            x_axis.append((bins[i] + bins[i + 1]) / 2)
+
+        # plotting the standard deviation and the lines on top of the histogram
+        error = np.std(data_list)
+        plt.errorbar(x_axis, n, yerr=error, fmt='o')
+        plt.plot(x_axis, n, linewidth=1, marker='4')
+
+        if plot_title != '':
+            plt.title(plot_title)
+            plot_filename = ("Plots\\" +
+                             plot_title.replace(' ', '_') + ".png")
+        else:
+            plt.title(str(plot_id) + (" instrumentos - Distribuicao dos EC's (" + str(len(data_list)) +
+                      " amostras)"))
+            plot_filename = ("Plots\\" + str(plot_id) + "_Insts_EC.png")
+
+        plt.ylabel('# de ocorrencias (Desv. Pad.: ' + str(round(error, 2)) + ')')
+        plt.xlabel("Valores Normalizados do Equilibrio de Coincidencias (EC)")
+        plt.grid()
+        plt.xlim(-1.0, 1.0)
+        plt.savefig(plot_filename, bbox_inches='tight', dpi=200)
+
+    def write_to_file(self, start_runtime, contents, write_mode='a'):
+        """
+        Writes contents to a file whose name is the provided runtime.
+        :param start_runtime: the time the test started running
+        :param contents: the contents to be written into the file
+        :param write_mode: (optional) the write mode (default: append)
+        """
+        with open(start_runtime + ".txt", write_mode) as f:
+            f.write("Filename\t\tMaxTrack\tNumInst\t\tTimeSig\t\tTPB\n")
+            f.write(contents)
 
 if __name__ == '__main__':
-	runtime = strftime("%Y_%m_%d___%H_%M_%S", localtime())
-	with open(runtime + ".txt", 'a') as f:
-			f.write("Filename\t\tMaxTrack\tNumInst\t\tTimeSig\t\tTPB\n")
+    plot_gen = PlotGenerator()
+    plot_gen.run()
 
-	for (dirpath, dirnames, filenames) in os.walk(windows_url):
-		for name in filenames:
-			#print "File: " + name
-			processor = MidiProcessor.MidiProcessor(os.path.join(dirpath + "\\" + name), name, runtime)
-			processor.processTracks()
-			processor.setup_variables()
-			processor.beatsByInstrument()
-			processor.createTimelines()
-			processor.writeResultsToFile()
-
-	# TODO: Divide in bars, check time signature data, do coincidences algorithm
+# TODO: Divide in bars, check time signature data
